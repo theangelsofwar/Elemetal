815e1d17888d46dade4b2f42ea1f1c590fc4d3c2
Angie <angiechangpagne@gmail.com>
2020-04-13 00:13:04 -0700

    gatsby

--- elemetal/gatsby-config.js
+++ elemetal/gatsby-config.js
@@ -1,8 +1,10 @@
+
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Elemetal`,
+    description: `Blade Runner Real World`,
+    author: `@angiechangpagne`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- elemetal/gatsby-ssr.js
+++ elemetal/gatsby-ssr.js
@@ -1,7 +1,8 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react';
+import Transition from './src/components/Transition';
 
-// You can delete this file if you're not using it
+export const wrapPageElement = ({element, props}) => {
+  return (
+    <Transition {...props}> {element} </Transition>
+  );
+};
