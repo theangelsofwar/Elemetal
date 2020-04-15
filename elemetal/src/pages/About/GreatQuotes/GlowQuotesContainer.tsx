ebced59b54a198cfe76621ea34b2efcd92dc21e7
Angie <angiechangpagne@gmail.com>
2020-04-15 01:39:04 -0700

    glowquote

---
+++ elemetal/src/pages/About/GreatQuotes/GlowQuotesContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import GlowQuote from './GlowQuote'
+
+const GlowQuotesContainer = () => {
+  return (
+    <div>
+      <GlowQuote />
+    </div>
+  )
+}
+
+export default GlowQuotesContainer
