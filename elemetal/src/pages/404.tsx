167ad1da1686b0a278a0b1861e74dea6439d7b2d
Angie <angiechangpagne@gmail.com>
2020-04-14 23:26:44 -0700

    vulcan

--- elemetal/src/pages/404.tsx
+++ elemetal/src/pages/404.tsx
@@ -5,31 +5,13 @@ import {Helmet} from 'react-helmet'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-// interface iProps {
-//   data: {
-//     site: {
-//       siteMetadata: {
-//         title: string
-//       }
-//     }
-//   }
-// }
-// export const pageQuery = graphql`
-//   query {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//   }
-// `
 const NotFoundPage = (): JSX.Element => (
   <Layout>
     <SEO title="404: Not found" />
     <Helmet>
       <title> Not Found </title>
     </Helmet>
-    <p>You just hit a void ether... get back safe</p>
+    <p>You just hit a void ether...get back safe</p>
   </Layout>
 )
 
