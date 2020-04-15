03f4892db2c65c258989f318316b090d18437bcd
Angie <angiechangpagne@gmail.com>
2020-04-15 00:16:51 -0700

    query

--- elemetal/src/pages/404.tsx
+++ elemetal/src/pages/404.tsx
@@ -1,17 +1,30 @@
+import {graphql} from 'gatsby'
 import React from 'react'
 import {Helmet} from 'react-helmet'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const NotFoundPage = (): JSX.Element => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <Helmet>
-      <title> Not Found </title>
-    </Helmet>
-    <p>You just hit a void ether...get back safe</p>
-  </Layout>
-)
+const NotFoundPage = (props: any): JSX.Element => {
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <Helmet>
+        <title> Exists? </title>
+      </Helmet>
+      <p>You just hit a void ether...get back safe</p>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
