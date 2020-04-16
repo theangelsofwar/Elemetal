4b9546fe4282b428e8ff51f100126b7470420086
Angie <angiechangpagne@gmail.com>
2020-04-16 01:50:55 -0700

    burlesque

--- elemetal/src/images/Img2.tsx
+++ elemetal/src/images/Img2.tsx
@@ -2,7 +2,7 @@ import {graphql} from 'gatsby'
 import Img from 'gatsby-image'
 import React from 'react'
 
-const Img2 = ({data}: any) => (
+const Img2 = ({data}: any): JSX.Element => (
   <div>
     <h1> 0101 </h1>
     <Img fixed={data.file.childImageSharp.fixed} />
