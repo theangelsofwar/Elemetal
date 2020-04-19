50afdc63c51e50ef34ba1b3e8af77ca756dc88ef
Angie <angiechangpagne@gmail.com>
2020-04-18 21:05:04 -0700

    switched between pages and components due to the path name

---
+++ elemetal/src/pages/archives.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//this will only be redirected from a specific page only..
+
+const Archives = (): JSX.Element => (
+  <React.Fragment>
+    <div id='archives'>
+      <p> Yve Vestal Titan Archives </p>
+    </div>
+  </React.Fragment>
+)
+
+export default Archives;
