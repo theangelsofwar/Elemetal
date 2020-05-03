73a99e87ea105e6447c9e9de4984aaaee438cb7a
Angie <angiechangpagne@gmail.com>
2020-05-03 13:39:41 -0700

    free

--- elemetal/src/components/Showcase/Showcase.tsx
+++ elemetal/src/components/Showcase/Showcase.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import '../../carraway/glow/glow.scss';
-
 import './styless.scss'; //add glow first
-
 
 const Showcase = (): JSX.Element => (
   <React.Fragment>
@@ -11,7 +8,7 @@ const Showcase = (): JSX.Element => (
     <h1> {'<'}Angie{'/>'} </h1>
     <p> <br/></p>
     <div id='glow' className='glow'>
-    <p> <h3>Software Developer </h3></p>
+    <p> <h3> Software Developer </h3> </p>
     <span id='lalocals' className='lalocals'> <p> Los Angeles, CA </p> </span>
     </div>
   </div>
