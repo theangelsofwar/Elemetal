4be9edde531889f7759277d7221baf176c143714
Angie <angiechangpagne@gmail.com>
2020-05-03 15:55:25 -0700

    monte

--- elemetal/gatsby-config.js
+++ elemetal/gatsby-config.js
@@ -123,6 +123,6 @@ module.exports = {
     `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 };
