78b2c67e28eac204ca1509e6be894a136939e45b
Angie <angiechangpagne@gmail.com>
2020-05-13 20:09:38 -0700

    new

--- elemetal/src/components/PageSlidebar/PageSlidebar.tsx
+++ elemetal/src/components/PageSlidebar/PageSlidebar.tsx
@@ -20,9 +20,9 @@ const PageSlidebar = (): JSX.Element => {
   return (
     <div id='scaleRotate' className='pageslidebar'>
     <Slider pageWrapId={'page-wrap'} outerContainerId={'carraway'} customBurgerIcon={ <img src={require("../../images/angelsOfWar.png")} alt=""/> }>
-      <a id='home' className='menu-item' href='/'> <span className='gradient'> </span><TransitionLink to='/' activeClass='active' className='transition-link'> Home </TransitionLink>  </a>
-      <a id='bio' className='menu-item' href= '/bio/'> <span className='gradient'>  </span> <TransitionLink to='/bio/' activeClass='active' className='transition-link'> Bio </TransitionLink> </a>
-      <a id='resume' className='menu-item' href='/Resume/'> <span className='gradient'>  </span> <TransitionLink to='/Resume/' activeClass='active' className='transition-link'> Resume </TransitionLink> </a>
+      <a id='home' className='menu-item' href='/'> <span className='gradient'>  </span>  <TransitionLink to='/' activeClass='active' className='transition-link'> Home </TransitionLink>  </a>
+      <a id='bio' className='menu-item' href= '/bio/'> <span className='gradient'>  </span>  <TransitionLink to='/bio/' activeClass='active' className='transition-link'> Bio        </TransitionLink> </a>
+      <a id='resume' className='menu-item' href='/Resume/'> <span className='gradient'>  </span>  <TransitionLink to='/Resume/' activeClass='active' className='transition-link'> Resume </TransitionLink> </a>
       <a id='opensourcegrid' className='menu-item' href='/opensourceGrid/'> <span className='gradient'> </span> <TransitionLink to='/opensourceGrid/' activeClass='active' className='transition-link'> Open Source </TransitionLink> </a>
       <a id='publicationsgrid' className='menu-item' href='/publicationsGrid/'> <span className='gradient'>  </span>  <TransitionLink to='/publicationsGrid/' activeClass='active' className='transition-link'> Publications </TransitionLink> </a>
       <a id='contactinqueries' className='menu-item' href='/contactInqueries'> <span className='gradient'>  </span>  <TransitionLink to='/contactInqueries' activeClass='active' className='transition-link' exit={{ zIndex: 2 }} > Query </TransitionLink> </a>
